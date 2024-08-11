1819707007c033c8ba3acf5357aa60f4bd0de856
RumkitPalu <rumahsakitsindhutrisnopalu@gmail.com>
2024-08-12 07:28:10 +0800

    fix bug token

--- src/utils/authToken.jsx
+++ src/utils/authToken.jsx
@@ -2,11 +2,13 @@ import Cookies from "js-cookie";
 
 export const authToken = async () => {
   const savedAuth = Cookies.get("auth");
-  if (savedAuth) {
+  if (savedAuth != undefined) {
     const { expiresAt } = await JSON.parse(savedAuth);
     if (Date.now() > expiresAt) {
       Cookies.remove("auth");
       window.location.reload();
     }
+  } else {
+    window.location.reload();
   }
 };
